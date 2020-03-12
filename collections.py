c061b78eccc5e794493ad2583a52a63d85cc7edc
Ilya Gotfryd <igotfryd@onshift.com>
2020-03-12 09:09:10 -0400

    Adding notes about collections

---
+++ collections.py
@@ -0,0 +1,12 @@
+# Let's cover collections and all the wonderful things we can do with them
+# While pandas and numpy do wonders and havevery potent c based paralellized code underneeth them
+# Python's native collectinons are quite magnificent and performance optimized themselves.
+# We will play with basics of the underlying language and see the power of what we can do right here
+
+## TUPLE
+
+## LIST
+
+## SET
+
+## DICTIONARY
