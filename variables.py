03f463d3d734ac705fb4582f30c1de32ddb7e482
Ilya Gotfryd <igotfryd@onshift.com>
2020-03-12 06:54:54 -0400

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,10 @@
+# let's cover literals and variables
+"""
+Python is a strong dynamic typed language, meaning it keeps true with assigned types,
+does type checking and does not perform type coersion.
+"""
+# declare an int
+# declare a float
+# declare a boolian
+# declare a string
+# Create a type error
