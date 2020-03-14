75ce0852a341522588f86d9cafc8fe78d052df00
Ilya Gotfryd <ilya.gotfryd@gmail.com>
2020-03-14 13:15:17 -0400

    adding strings plus rename

---
+++ strings.py
@@ -0,0 +1,20 @@
+"""
+In this module we are going to be covering strings, string formatting and string manipilation.
+This comment is a multiline string.
+"""
+
+# Single line string literal
+
+# Multiline string literal
+
+# f-string and f-string formatting
+
+# splitting a string
+
+# joining a string
+
+# stripping leading and trainling spaces
+
+# case manipulation
+
+# let's build a random IP address
